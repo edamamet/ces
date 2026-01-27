6324b1d2b694a80e4ba596ea79e86996b9477d09
edamamet <adamtam2004@outlook.com>
2026-01-27 18:19:34 -0500

    changed font size and color

--- src/ces_config.h
+++ src/ces_config.h
@@ -3,13 +3,13 @@
 #include "SDL3/SDL_pixels.h"
 
 constexpr SDL_Color WHITE = {255,255,255,255};
-constexpr SDL_Color BLACK = {0,0,0,255};
+constexpr SDL_Color BLACK = {54,51,47,255};
 constexpr SDL_Color DEBUG_BG = {25,25,25,255};
 constexpr SDL_Color DEBUG_FG = {0,255,255,255};
 
 constexpr const char* FONT_REGULAR_PATH = "asset/font/Zalando_Sans_Expanded/ZalandoSansExpanded-VariableFont_wght.ttf";
 constexpr const char* FONT_ITALIC_PATH = "asset/font/Zalando_Sans_Expanded/ZalandoSansExpanded-Italic-VariableFont_wght.ttf";
-constexpr int FONT_SIZE = 24;
+constexpr int FONT_SIZE = 20;
 constexpr float MAX_SCORE = 10.0f;
 
 constexpr double MAX_ADJUSTMENT_TIME = 4;
